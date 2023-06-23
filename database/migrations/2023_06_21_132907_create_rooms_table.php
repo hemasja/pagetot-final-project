24f07f264cc93a59fe6b5b9daf77eeb666cf1fc0
Muhammad Hemas Julang Arrasy <91818681+hemasja@users.noreply.github.com>
2023-06-23 21:55:00 +0700

    EDIT: Editing database migration code

--- database/migrations/2023_06_21_132907_create_rooms_table.php
+++ database/migrations/2023_06_21_132907_create_rooms_table.php
@@ -10,7 +10,7 @@ class CreateRoomsTable extends Migration
     {
         Schema::create('rooms', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('jenis_kamar');
             $table->string('description');
             $table->timestamps();
         });
