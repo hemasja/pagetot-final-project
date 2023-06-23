53598ee81b8bc7ddcc60da7edd2d4bff5b5468fe
Ananda Fitra <anandadf@student.ub.ac.id>
2023-06-23 10:07:54 +0700

    FIX: Restore missing code due to rebase

--- app/Http/Controllers/CctvController.php
+++ app/Http/Controllers/CctvController.php
@@ -1,3 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Cctv;
+use Illuminate\Http\Request;
+
+class CctvController extends Controller
+{
+    public function __invoke() {
+        $cctvs = Cctv::getAll();
+        return view('cctv.list', ['cctvs' => $cctvs]);
+    }
 
     public function monitorCctv(Request $request) {
         $cctv = Cctv::getById($request->get('id'));
